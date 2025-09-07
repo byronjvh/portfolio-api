4e0ee57e50efac6595130d08cc8e29bfa9abd416
byronjvh <byronjvh02@gmail.com>
2025-09-07 11:29:48 -0600

    added debug

--- api/contact.js
+++ api/contact.js
@@ -41,6 +41,7 @@ export default async function handler(req, res) {
 
         return res.status(200).json({ message: "✅ Mensaje enviado con éxito", data });
     } catch (err) {
+        console.error(err);
         return res.status(500).json({ message: "❌ Error interno", error: String(err) });
     }
 }
